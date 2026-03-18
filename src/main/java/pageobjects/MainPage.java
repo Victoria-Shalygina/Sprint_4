8d64d862906789b0d18b53c6bf839d0b30e7114d
Victoria-Shalygina <dimid3518@gmail.com>
2026-03-18 19:23:47 +0000

    Update MainPage.java

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -12,26 +12,7 @@ public class MainPage {
     }
 
     private By orderTopButton = By.className("Button_Button__ra12g");
-
     private By orderBottomButton = By.xpath(".//button[text()='Заказать']");
-
-    private By question1 = By.id("accordion__heading-0");
-    private By question2 = By.id("accordion__heading-1");
-    private By question3 = By.id("accordion__heading-2");
-    private By question4 = By.id("accordion__heading-3");
-    private By question5 = By.id("accordion__heading-4");
-    private By question6 = By.id("accordion__heading-5");
-    private By question7 = By.id("accordion__heading-6");
-    private By question8 = By.id("accordion__heading-7");
-
-    private By answer1 = By.id("accordion__panel-0");
-    private By answer2 = By.id("accordion__panel-1");
-    private By answer3 = By.id("accordion__panel-2");
-    private By answer4 = By.id("accordion__panel-3");
-    private By answer5 = By.id("accordion__panel-4");
-    private By answer6 = By.id("accordion__panel-5");
-    private By answer7 = By.id("accordion__panel-6");
-    private By answer8 = By.id("accordion__panel-7");
 
     public void clickOrderTop() {
         driver.findElement(orderTopButton).click();
@@ -42,10 +23,12 @@ public class MainPage {
     }
 
     public void clickQuestion(int index) {
-        driver.findElement(By.id("accordion__heading-" + index)).click();
+        By questionLocator = By.id("accordion__heading-" + index);
+        driver.findElement(questionLocator).click();
     }
 
     public String getAnswerText(int index) {
-        return driver.findElement(By.id("accordion__panel-" + index)).getText();
+        By answerLocator = By.id("accordion__panel-" + index);
+        return driver.findElement(answerLocator).getText();
     }
 }
